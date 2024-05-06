2448a35774867f312acb0d934165f65d607d44ba
Osman Erdem Kılıç <osman_erdem1200@hotmail.com>
2024-05-06 11:36:37 +0300

    Weather App

---
+++ constants/index.js
@@ -0,0 +1,23 @@
+export const apiKey="6197a983ca814de7baf113204232103"
+
+export const weatherImages = {
+    'Partly cloudy' : require('../assets/images/partlycloudy.png'),
+    'Moderate rain': require('../assets/images/moderaterain.png'), //okey
+    'Patchy rain possible' : require('../assets/images/moderaterain.png'), //okey
+    'Patchy rain nearby' : require('../assets/images/moderaterain.png'), //okey
+    'Light rain shower' : require('../assets/images/moderaterain.png'), //okey
+    'Sunny' : require('../assets/images/sun.png'), //okey
+    'Clear' : require('../assets/images/sun.png'),
+    'Overcast' : require('../assets/images/cloud.png'),
+    'Cloudy' : require('../assets/images/cloud.png'),
+    'Light rain': require('../assets/images/moderaterain.png'), //okey
+    'Moderate rain at times' : require('../assets/images/moderaterain.png'), //okey
+    'Heavy rain' : require('../assets/images/heavyrain.png'),
+    'Heavy rain at times' : require('../assets/images/heavyrain.png'),
+    'Moderate or heavy freezing rain' : require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain shower' : require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain with thunder' : require('../assets/images/heavyrain.png'),
+    'other' : require('../assets/images/moderaterain.png')
+
+
+}
---
+++ navigation/AppNavigation.js
@@ -0,0 +1,21 @@
+import { View, Text, LogBox } from 'react-native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import HomeScreen from '../screens/HomeScreen'
+
+const Stack = createNativeStackNavigator()
+
+LogBox.ignoreLogs([
+    'Non-serializable values were found in the navigation state',
+])
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}} />
+        </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
